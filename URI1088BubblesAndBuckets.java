51959c18346cd56b5abc3abae5ffcc0348545657
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-06-07 14:15:22 -0500

    rename variables

--- URI1088BubblesAndBuckets.java
+++ URI1088BubblesAndBuckets.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 /**
  * See
@@ -13,36 +14,34 @@ import java.io.PrintWriter;
  */
 public class URI1088BubblesAndBuckets {
 
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static PrintWriter out = new PrintWriter(System.out);
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter out = new PrintWriter(System.out);
 
-    public static void main(String[] args) throws IOException {
-        String l;
-        String P[];
-        int N, swaps, pos;
-        while (!(l = in.readLine()).equals("0")) {
-            P = l.split("\\s");
-            N = Integer.parseInt(P[0]);
-            int[] X = new int[N];
-            int[] aux = new int[N];
-            for (int i = 0; i < N; i++) {
-                pos = Integer.parseInt(P[i + 1]) - 1;
-                X[pos] = i;
-                aux[i] = pos;
-            }
-            swaps = 0;
-            for (int i = 0; i < N - 1; i++) {
-                if (X[i] != i) {
-                    X[aux[i]] = X[i];
-                    aux[X[i]] = aux[i];
-//                    X[i] = i; 
-//                    aux[i] = i;
-                    swaps++;
-                }
-            }
-            out.println(swaps % 2 == 0 ? "Carlos" : "Marcelo");
-        }
-        out.close();
-    }
+	public static void main(String[] args) throws IOException {
+		String l;
+		String P[];
+		int N, swaps, pos;
+		while (!(l = in.readLine()).equals("0")) {
+			P = l.split("\\s");
+			N = Integer.parseInt(P[0]);
+			int[] keys = new int[N];
+			int[] values = new int[N];
+			for (int i = 0; i < N; i++) {
+				pos = Integer.parseInt(P[i + 1]) - 1;
+				keys[pos] = i;
+				values[i] = pos;
+			}
+			swaps = 0;
+			for (int i = 0; i < N - 1; i++) {
+				if (keys[i] != i) {
+					keys[values[i]] = keys[i];
+					values[keys[i]] = values[i];
+					swaps++;
+				}
+			}
+			out.println(swaps % 2 == 0 ? "Carlos" : "Marcelo");
+		}
+		out.close();
+	}
 
 }
