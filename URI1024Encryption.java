e469bbf1f8e8db5ea0dbc905b14f8b1e59c13ac6
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-06-07 13:51:39 -0500

    create URI1024Encryption.java

---
+++ URI1024Encryption.java
@@ -0,0 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+/**
+ * See
+ * <a href="https://www.urionlinejudge.com.br/judge/en/problems/view/1024">Encryption</a>
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class URI1024Encryption {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) throws IOException {
+		int N = readInt(), half, length;
+		char[] M;
+		char temp;
+		while (N-- > 0) {
+			M = read().toCharArray();
+			length = M.length;
+			half = length / 2;
+			for (int i = 0; i < length; i++) {
+				if (Character.isLetter(M[i])) {
+					M[i] = (char) ((int) M[i] + 3);
+				}
+			}
+			for (int i = 0; i < half; i++) {
+				temp = M[i];
+				M[i] = M[M.length - i - 1];
+				M[M.length - i - 1] = temp;
+			}
+			for (int i = (half); i < length; ++i) {
+				M[i] = (char) ((int) M[i] - 1);
+			}
+			for (char Mi : M) {
+				out.print(Mi);
+			}
+			out.println();
+		}
+		out.close();
+	}
+
+	private static String read() throws IOException {
+		return in.readLine();
+	}
+
+	private static int readInt() throws IOException {
+		return Integer.parseInt(in.readLine());
+	}
+
+}
