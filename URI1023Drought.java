545eed480236d51a5c18d132cf6d3d287423b6d4
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-06-19 09:21:20 -0500

    Fast I/O

--- URI1023Drought.java
+++ URI1023Drought.java
@@ -1,9 +1,15 @@
 
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Writer;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,26 +21,27 @@ import java.util.TreeMap;
  */
 public class URI1023Drought {
 
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static PrintWriter out = new PrintWriter(System.out);
+    static InputReader in = new InputReader(System.in);
+    static OutputWriter out = new OutputWriter(System.out);
 
     public static void main(String[] args) throws IOException {
         String aux;
         int N, X, Y, cityNumber = 1, totalResidents, totalConsumption, roundConsumption;
         boolean firstCase = true;
         int[] P;
-        while (!(aux = read()).equals("0")) {
+        double ans;
+        while (!(aux = in.readString()).equals("0")) {
             if (firstCase) {
                 firstCase = false;
             } else {
-                out.println();
+                out.printLine();
             }
             totalResidents = 0;
             totalConsumption = 0;
             Map<Integer, Integer> relation = new HashMap<>();
-            N = toInt(aux);
+            N = Integer.parseInt(aux);
             while (N-- > 0) {
-                P = readArray();
+                P = IOUtils.readIntArray(in, 2);
                 X = P[0];
                 Y = P[1];
                 totalResidents += X;
@@ -45,7 +52,7 @@ public class URI1023Drought {
                 }
                 relation.put(roundConsumption, X);
             }
-            out.println("Cidade# " + cityNumber++ + ":");
+            out.printLine("Cidade# " + cityNumber++ + ":");
             aux = "";
             Map<Integer, Integer> treeMap = new TreeMap<>(relation);
             for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
@@ -53,28 +60,152 @@ public class URI1023Drought {
                 Integer value = entry.getValue();
                 aux += value + "-" + key + " ";
             }
-            out.println(aux.trim());
-            out.printf("Consumo medio: %.2f m3.\n", Math.floor(100 * (((100 * (double) totalConsumption) / (100 * (double) totalResidents)))) / 100);
+            out.printLine(aux.trim());
+            ans = Math.floor(100 * (((100 * (double) totalConsumption) / (100 * (double) totalResidents)))) / 100;
+            out.printLine("Consumo medio: " + String.format("%.2f", ans) + " m3.");
 
         }
+        out.flush();
         out.close();
     }
 
-    private static String read() throws IOException {
-        return in.readLine();
-    }
-
-    private static int[] readArray() throws IOException {
-        String[] line = in.readLine().split("\\s");
-        int l = line.length;
-        int[] a = new int[l];
-        for (int i = 0; i < l; i++) {
-            a[i] = Integer.parseInt(line[i]);
+    public static double round(double value, int places) {
+        if (places < 0) {
+            throw new IllegalArgumentException();
         }
-        return a;
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+}
+
+class InputReader {
+
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+    private SpaceCharFilter filter;
+
+    public InputReader(InputStream stream) {
+        this.stream = stream;
     }
 
-    private static int toInt(String s) {
-        return Integer.parseInt(s);
+    public int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        }
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0) {
+                return -1;
+            }
+        }
+        return buf[curChar++];
     }
+
+    public int readInt() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public String readString() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    public boolean isSpaceChar(int c) {
+        if (filter != null) {
+            return filter.isSpaceChar(c);
+        }
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    public String next() {
+        return readString();
+    }
+
+    public interface SpaceCharFilter {
+
+        public boolean isSpaceChar(int ch);
+    }
+}
+
+class OutputWriter {
+
+    private final PrintWriter writer;
+
+    public OutputWriter(OutputStream outputStream) {
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+    public OutputWriter(Writer writer) {
+        this.writer = new PrintWriter(writer);
+    }
+
+    public void print(Object... objects) {
+        for (int i = 0; i < objects.length; i++) {
+            if (i != 0) {
+                writer.print(' ');
+            }
+            writer.print(objects[i]);
+        }
+    }
+
+    public void printLine(Object... objects) {
+        print(objects);
+        writer.println();
+    }
+
+    public void close() {
+        writer.close();
+    }
+
+    public void flush() {
+        writer.flush();
+    }
+
+}
+
+class IOUtils {
+
+    public static int[] readIntArray(InputReader in, int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = in.readInt();
+        }
+        return array;
+    }
+
 }
