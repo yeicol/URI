0239519c8cd9d20d930a26877abc3ff569ef2873
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-09-25 13:38:51 -0500

    create URI1285DifferentDigits.java

---
+++ URI1285DifferentDigits.java
@@ -0,0 +1,131 @@
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * See
+ * <a href="https://www.urionlinejudge.com.br/judge/en/problems/view/1285">Different
+ * Digits</a>
+ *
+ * @author Brian Yeicol Restrepo Tangarife
+ */
+public class URI1285DifferentDigits {
+
+    static Reader in = new Reader(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws IOException {
+        int N, M, s;
+        while (in.hasNext()) {
+            N = in.nextInt();
+            M = in.nextInt();
+            s = 0;
+            for (int i = N; i <= M; i++) {
+                if(!hasRepeatedChars(String.valueOf(i))){
+                    s++;
+                }
+            }
+            out.println(s);
+        }
+        in.close();
+        out.close();
+    }
+
+    private static boolean hasRepeatedChars(String i) {
+        return i.matches(".*(.).*\\1.*");
+    }
+
+    static class Reader implements Closeable {
+
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public Reader(InputStream input) {
+            reader = new BufferedReader(
+                    new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private StringTokenizer getTokenizer() throws IOException {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                String line = nextLine();
+                if (line == null) {
+                    return null;
+                }
+                tokenizer = new StringTokenizer(line);
+            }
+            return tokenizer;
+        }
+
+        public boolean hasNext() throws IOException {
+            return getTokenizer() != null;
+        }
+
+        public String next() throws IOException {
+            return hasNext() ? tokenizer.nextToken() : null;
+        }
+
+        public String nextLine() throws IOException {
+            tokenizer = null;
+            return reader.readLine();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public String[] nextStringArray(int size) throws IOException {
+            String[] array = new String[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = next();
+            }
+            return array;
+        }
+
+        public int[] nextIntArray(int size) throws IOException {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+
+        public long[] nextLongArray(int size) throws IOException {
+            long[] array = new long[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = nextLong();
+            }
+            return array;
+        }
+
+        public double[] nextDoubleArray(int size) throws IOException {
+            double[] array = new double[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = nextDouble();
+            }
+            return array;
+        }
+
+        @Override
+        public void close() throws IOException {
+            tokenizer = null;
+            reader.close();
+        }
+    }
+}
